e3f3e43e152efe4e1319896d3e9cd0127c5f2660
b-willii <60661937+b-willii@users.noreply.github.com>
2022-08-31 18:42:05 +0100

    Update ToDoAdapter.java
    Added reference/citation

--- app/src/main/java/com/example/taskappv2/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/taskappv2/Adapter/ToDoAdapter.java
@@ -1,3 +1,14 @@
+/***************************************************************************************
+ *    References:
+ *
+ *    Title: To Do List App Android Studio Tutorial Series
+ *    Author: Mohit Singh - Penguin Coders
+ *    Date: 2020
+ *    Code version: 1.0
+ *    Availability: https://www.youtube.com/playlist?list=PLzEWSvaHx_Z2MeyGNQeUCEktmnJBp8136
+ *
+ ***************************************************************************************/
+
 package com.example.taskappv2.Adapter;
 
 import android.content.Context;
